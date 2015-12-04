38265087a9b9b611030cb564d27a4ad4e00a2092
Kiehls <nonestrike@gmail.com>
2015-12-04 21:09:55 +0900

    code refactoring

---
+++ app/src/main/java/embedded/kookmin/ac/kr/database/InfoClass.java
@@ -0,0 +1,14 @@
+package embedded.kookmin.ac.kr.database;
+
+/**
+ * Created by 승수 on 2015-11-22.
+ */
+public class InfoClass {
+    public int _id;
+    public String name;
+
+    public InfoClass(int id, String name) {
+        this._id = id;
+        this.name = name;
+    }
+}
