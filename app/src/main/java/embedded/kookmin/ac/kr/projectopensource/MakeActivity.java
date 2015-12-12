fb7ff0314039fc4e873239b9691aaaf30fe08d58
Kiehls <nonestrike@gmail.com>
2015-12-12 21:12:55 +0900

    add refresh & change editText to CheckBox

--- app/src/main/java/embedded/kookmin/ac/kr/projectopensource/MakeActivity.java
+++ app/src/main/java/embedded/kookmin/ac/kr/projectopensource/MakeActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,18 +16,27 @@ import java.util.ArrayList;
 
 import embedded.kookmin.ac.kr.database.DbOpenHelper;
 
-public class MakeActivity extends AppCompatActivity implements View.OnClickListener{
+public class MakeActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener{
 
-    // Database 관련 객체들
+    // Database object
     public DbOpenHelper db;
     SQLiteDatabase database;
 
     // layout object
-    EditText needPerson;
+    EditText title;
     EditText numPerson;
     EditText comment;
     Button bInsert;
     Button bBack;
+    CheckBox option1; //개발자
+    CheckBox option2; //디자이너
+    CheckBox option3; //기획자
+    CheckBox option4; //기타
+
+    int opt1 = 0;
+    int opt2 = 0;
+    int opt3 = 0;
+    int opt4 = 0;
 
     ArrayAdapter<String> DbAdapter;
     ArrayList<String> teamList;
@@ -36,36 +47,35 @@ public class MakeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make);
-        Intent intent = this.getIntent();
+        findviewById();
+
+        Intent intent = this.getIntent();//from ListActivity
         tableName = intent.getStringExtra("contestTable");
+
         db = new DbOpenHelper(this);
         database = db.getReadableDatabase();
 
-
-        needPerson = (EditText) findViewById(R.id.whos);
-        numPerson = (EditText) findViewById(R.id.numbers);
-        comment = (EditText) findViewById(R.id.editText_content);
-
-        bInsert = (Button) findViewById(R.id.button_ok);
         bInsert.setOnClickListener(this);
-
-        bBack = (Button) findViewById(R.id.button_cancel);
         bBack.setOnClickListener(this);
+
+        option1.setOnCheckedChangeListener(this);
+        option2.setOnCheckedChangeListener(this);
+        option3.setOnCheckedChangeListener(this);
+        option4.setOnCheckedChangeListener(this);
 
         // Create listview
         teamList = new ArrayList<String>();
     }
-
     //Button 클릭 이벤트
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_ok:
-                String need = needPerson.getText().toString();
+                String topic = title.getText().toString();
                 int num = Integer.parseInt(numPerson.getText().toString());
                 String comm = comment.getText().toString();
 
-                db.insertContestInfo(tableName, need, num, comm);
+                db.insertContestInfo(tableName, topic, num, comm, opt1, opt2, opt3, opt4);
 
                 Toast.makeText(getApplicationContext(), "팀 등록이 완료되었습니다.", Toast.LENGTH_SHORT).show();
 
@@ -75,5 +85,39 @@ public class MakeActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
                 break;
         }
+    }
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(buttonView.getId() == R.id.developCheck) {
+            if(isChecked) {
+                opt1 = 1;
+            }
+        }
+        if(buttonView.getId() == R.id.designCheck) {
+            if(isChecked) {
+                opt2 = 1;
+            }
+        }
+        if(buttonView.getId() == R.id.planCheck) {
+            if(isChecked) {
+                opt3 = 1;
+            }
+        }
+        if(buttonView.getId() == R.id.etcCheck) {
+            if(isChecked) {
+                opt4 = 1;
+            }
+        }
+    }
+    public void findviewById() {
+        title = (EditText) findViewById(R.id.title);
+        numPerson = (EditText) findViewById(R.id.numbers);
+        comment = (EditText) findViewById(R.id.editText_content);
+        bInsert = (Button) findViewById(R.id.button_ok);
+        bBack = (Button) findViewById(R.id.button_cancel);
+        option1 = (CheckBox) findViewById(R.id.developCheck);
+        option2 = (CheckBox) findViewById(R.id.designCheck);
+        option3 = (CheckBox) findViewById(R.id.planCheck);
+        option4 = (CheckBox) findViewById(R.id.etcCheck);
     }
 }
